4e6e2c1904a7a30d82a9d431d0609e78a2ba3af8
MarMaps <mgolubeva468@gmail.com>
2024-07-17 11:55:11 +0500

    add 4

---
+++ геометрич задачи/4/4_center_point.c
@@ -0,0 +1,19 @@
+//находит координаты центра прямоугольника
+
+#include <stdio.h>
+
+void center(int xlt, int ylt, int xrb, int yrb, int *pxc, int *pyc)
+{
+  *pxc = (xlt + xrb) / 2;
+  *pyc = (ylt + yrb) / 2;
+}
+
+int main()
+{
+  int xlt, ylt, xrb, yrb, x, y;
+  scanf("%d%d%d%d", &xlt, &ylt, &xrb, &yrb);
+  
+  center(xlt, ylt, xrb, yrb, &x, &y);
+  printf("%d %d\n", x, y);
+  return 0;
+}
