ca511ac2eda2cfcf851b61636ca4f7cb5929fd5f
MarMaps <mgolubeva468@gmail.com>
2024-07-25 13:59:20 +0500

    верх левая и пр нижняя точка

---
+++ структуры/rect_ltrb/rect_ltrb.c
@@ -0,0 +1,20 @@
+//координаты верхней левой и правой нижней точки прямоугольника
+
+#include <stdio.h>
+
+struct Point {
+    int x, y;
+};
+
+struct Rect1 {
+    struct Point lt;
+    struct Point rb;
+};
+
+int main()
+{
+	struct Rect1 b = {{-7, 5}, {5, -3}};
+	printf("%d %d %d %d\n", b.lt.x, b.lt.y, b.rb.x, b.rb.y);
+
+	return 0;
+}
