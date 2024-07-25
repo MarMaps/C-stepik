2dc88d84ee85e2ae5e1a4eb3751839840d300c66
MarMaps <mgolubeva468@gmail.com>
2024-07-25 13:49:31 +0500

    представление часов

---
+++ структуры/time_def/time_def.c
@@ -0,0 +1,15 @@
+// хранение и представление показаний часов
+
+#include <stdio.h>
+
+struct Time {
+    int h, m;
+};
+
+int main()
+{
+    struct Time t;
+    scanf("%d:%d", &t.h, &t.m);
+    printf("%02d:%02d\n", t.h, t.m);
+    return 0;
+}
